eeccf8287b5d3e9ed453106917c5f05c4436a613
JustCluby <jay.shen918@gmail.com>
2025-09-15 18:31:35 +1000

    Soap bar now decreases over time

--- Assets/Scripts/UI/progressBar.cs
+++ Assets/Scripts/UI/progressBar.cs
@@ -6,27 +6,31 @@ using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 using UnityEngine.UI;
 
-[ExecuteInEditMode()]
+//[ExecuteInEditMode()]
 public class progressBar : MonoBehaviour
 {
-    [SerializeField] private UnityEngine.UI.Image mask;   // UI Image reference
+    [SerializeField] private UnityEngine.UI.Image mask;
     [SerializeField] private float max = 100f;
     [SerializeField] private float cur = 0f;
+    [SerializeField] private float drainRate = 14.5f;
 
     void Start()
     {
 
     }
 
-    void Update()
+    private void Update()
     {
+        cur -= drainRate * Time.deltaTime; //decreases by the drainRate/per second
+        cur = Mathf.Clamp(cur, 0, max);
+
         UpdateBar();
     }
 
     private void UpdateBar()
     {
-        float fillamt = cur / max;
-        mask.fillAmount = fillamt;
+        float fillAmount = cur / max;
+        mask.fillAmount = fillAmount;
     }
 
     public void SetValue(float value)
